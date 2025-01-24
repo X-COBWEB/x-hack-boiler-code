7ec6907e1581ee8fe31455d2551ea23b5458c157
Manee Das Shrestha <shresthamaneedas852@gmail.com>
2025-01-24 09:52:20 +0545

    navbar

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+
+export default function Home() {
+  return (
+    <>
+    <div>manee</div>
+    
+    </>
+  );
+}
